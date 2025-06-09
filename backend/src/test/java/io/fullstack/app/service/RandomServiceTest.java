2800b934625ab059458e6331eb88738692b428e5
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-06-09 22:16:18 +0400

    update ci

--- backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
+++ backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
@@ -1,12 +1,14 @@
 package io.fullstack.app.service;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomServiceTest {
     @Test
+    @DisplayName("should be equal to 4")
     void test() {
         assertEquals(4, 2 + 2);
     }
