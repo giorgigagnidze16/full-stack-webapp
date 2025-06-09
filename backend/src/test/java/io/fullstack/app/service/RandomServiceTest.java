ac436a1542b39679b91c380b661d126d6ff82107
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-06-09 22:17:40 +0400

    add other assertion

--- backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
+++ backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
@@ -8,8 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomServiceTest {
     @Test
-    @DisplayName("should be equal to 4")
+    @DisplayName("should be equal to 4 and 5")
     void test() {
         assertEquals(4, 2 + 2);
+        assertEquals(5, 2 + 3);
     }
 }
