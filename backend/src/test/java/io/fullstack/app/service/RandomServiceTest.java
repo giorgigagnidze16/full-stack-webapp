79444595aec9ba0949be3cb9e6e222da907810bb
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-06-09 22:15:23 +0400

    random test

---
+++ backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
@@ -0,0 +1,13 @@
+package io.fullstack.app.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RandomServiceTest {
+    @Test
+    void test() {
+        assertEquals(4, 2 + 2);
+    }
+}
