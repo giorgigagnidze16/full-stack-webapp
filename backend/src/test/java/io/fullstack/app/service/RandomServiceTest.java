a6dc6bb89eb61de56b9d5fa258d989bab5f8ab09
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-06-09 22:38:09 +0400

    add random test

--- backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
+++ backend/src/test/java/io/fullstack/app/service/RandomServiceTest.java
@@ -8,9 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomServiceTest {
     @Test
-    @DisplayName("should be equal to 4 and 5")
+    @DisplayName("should be equal to 4, 5 and 6")
     void test() {
         assertEquals(4, 2 + 2);
         assertEquals(5, 2 + 3);
+        assertEquals(6, 3 + 3);
     }
 }
