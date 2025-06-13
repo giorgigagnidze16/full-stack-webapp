7b4cf4cb3bed68adb81ace86a1de3bcf86dc4471
Giorgi.Gagnidze <giorgi.gagnidze@voidaint.com>
2025-06-13 13:14:38 +0400

    change filter logging to debug level

--- backend/src/main/java/io/fullstack/app/filter/LoggingFilter.java
+++ backend/src/main/java/io/fullstack/app/filter/LoggingFilter.java
@@ -4,19 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Request URI: " + request.getRequestURI());
-        System.out.println("Authorization header: " + request.getHeader("Authorization"));
+        log.debug("Request URI: {}", request.getRequestURI());
+        log.debug("Authorization header: {}", request.getHeader("Authorization"));
         filterChain.doFilter(request, response);
     }
 }
